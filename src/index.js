b4ee3f54db35f866afeec75b7db72900e06ed9c8
hyunwoo_park5 <hyunwoo_park5@tmax.co.kr>
2022-09-05 16:46:31 +0900

    feat: 타입 이름 변경 및 사용하지 않는 코드 삭제

--- src/index.js
+++ src/index.js
@@ -1,57 +1,5 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
- */
-function Book(title, author) {}
-
-/**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
- */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-  throw new Error("must be implemented by subclass!");
-};
-
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-  return false;
-};
-
-/**
- * @typedef {Object} Point
+ * @typedef {Object} TodoProps
  * @property {string} id - The title of the App.
  * @property {string} contents - The contents of the App.
  * @property {boolean} completed - The author of the App.
@@ -72,27 +20,21 @@ Milk.prototype.isSolid = function () {
 function TODO(Point) {}
 
 /**
- * @return {Point}
+ * @return {TodoProps}
  */
 TODO.prototype.CREATE = function ({ id, contents, category, tags }) {};
 
 /**
- * @return {Point}
+ * @return {TodoProps}
  */
 TODO.prototype.READ = function ({ id }) {};
 
 /**
- * @return {Point}
+ * @return {TodoProps}
  */
-TODO.prototype.UPDATE = function ({
-  id,
-  contents,
-  completed,
-  category,
-  tags,
-}) {};
+TODO.prototype.UPDATE = function (TodoProps) {};
 
 /**
- * @return {Point}
+ * @return {TodoProps}
  */
 TODO.prototype.DELETE = function ({ id, category, tags }) {};
